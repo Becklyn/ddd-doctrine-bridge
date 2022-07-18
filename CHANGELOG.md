bfe8702bdb71d7438469f679f46633b71e117f8e
Marko Vujnovic <mv@becklyn.com>
2022-07-18 14:45:33 +0200

    Fixed migration platform requirements

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+2.2.2
+=====
+
+* (bug) Fixed migration requirements to work as intended.
+
 2.2.1
 =====
 
--- src/Events/Infrastructure/DoctrineMigrations/Version20201005084703.php
+++ src/Events/Infrastructure/DoctrineMigrations/Version20201005084703.php
@@ -23,13 +23,17 @@ final class Version20201005084703 extends AbstractMigration
 
     public function up(Schema $schema) : void
     {
-        $platform = get_class($this->connection->getDatabasePlatform());
+        $platform = $this->connection->getDatabasePlatform();
+        $isMySql = $platform instanceof MySQLPlatform;
+        $isOracle = $platform instanceof OraclePlatform;
+        $isSqlite = $platform instanceof SqlitePlatform;
+
         $this->skipIf(
-            !in_array($platform, [SqlitePlatform::class, MySQLPlatform::class, OraclePlatform::class]),
+            !$isMySql && !$isOracle && !$isSqlite,
             'Migration can only be executed safely on \'MySQL\',  \'SQLite\' or \'Oracle\'.'
         );
 
-        if ($platform === MySQLPlatform::class) {
+        if ($isMySql) {
             $this->addSql('CREATE TABLE event_store (event_id VARCHAR(36) NOT NULL, aggregate_id VARCHAR(36) DEFAULT NULL, event_type_id VARCHAR(36) DEFAULT NULL, version INT NOT NULL, raised_ts DATETIME(6) NOT NULL, data LONGTEXT NOT NULL, created_ts DATETIME(6) NOT NULL, INDEX IDX_BE4CE95BD0BBCCBE (aggregate_id), INDEX IDX_BE4CE95B401B253C (event_type_id), PRIMARY KEY(event_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
             $this->addSql('CREATE TABLE event_store_aggregate_types (id VARCHAR(36) NOT NULL, name VARCHAR(255) NOT NULL, created_ts DATETIME(6) NOT NULL, UNIQUE INDEX uniq_aggregate_type_name (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
             $this->addSql('CREATE TABLE event_store_aggregates (id VARCHAR(36) NOT NULL, aggregate_type_id VARCHAR(36) DEFAULT NULL, version INT NOT NULL, created_ts DATETIME(6) NOT NULL, updated_ts DATETIME(6) NOT NULL, INDEX IDX_3E42E1EAC816D3AF (aggregate_type_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -37,7 +41,7 @@ final class Version20201005084703 extends AbstractMigration
             $this->addSql('ALTER TABLE event_store ADD CONSTRAINT FK_BE4CE95BD0BBCCBE FOREIGN KEY (aggregate_id) REFERENCES event_store_aggregates (id)');
             $this->addSql('ALTER TABLE event_store ADD CONSTRAINT FK_BE4CE95B401B253C FOREIGN KEY (event_type_id) REFERENCES event_store_event_types (id)');
             $this->addSql('ALTER TABLE event_store_aggregates ADD CONSTRAINT FK_3E42E1EAC816D3AF FOREIGN KEY (aggregate_type_id) REFERENCES event_store_aggregate_types (id)');
-        } else if ($platform === SqlitePlatform::class) {
+        } else if ($isSqlite) {
             $this->addSql('CREATE TABLE event_store (event_id VARCHAR(36) NOT NULL, aggregate_id VARCHAR(36) DEFAULT NULL, event_type_id VARCHAR(36) DEFAULT NULL, version INTEGER NOT NULL, raised_ts DATETIME(6) NOT NULL, data CLOB NOT NULL, created_ts DATETIME(6) NOT NULL, PRIMARY KEY(event_id))');
             $this->addSql('CREATE INDEX IDX_BE4CE95BD0BBCCBE ON event_store (aggregate_id)');
             $this->addSql('CREATE INDEX IDX_BE4CE95B401B253C ON event_store (event_type_id)');
@@ -65,13 +69,17 @@ final class Version20201005084703 extends AbstractMigration
 
     public function down(Schema $schema) : void
     {
-        $platform = get_class($this->connection->getDatabasePlatform());
+        $platform = $this->connection->getDatabasePlatform();
+        $isMySql = $platform instanceof MySQLPlatform;
+        $isOracle = $platform instanceof OraclePlatform;
+        $isSqlite = $platform instanceof SqlitePlatform;
+
         $this->skipIf(
-            !in_array($platform, [SqlitePlatform::class, MySQLPlatform::class, OraclePlatform::class]),
+            !$isMySql && !$isOracle && !$isSqlite,
             'Migration can only be executed safely on \'MySQL\',  \'SQLite\' or \'Oracle\'.'
         );
 
-        if ($platform === MySQLPlatform::class) {
+        if ($isMySql) {
             $this->addSql('ALTER TABLE event_store_aggregates DROP FOREIGN KEY FK_3E42E1EAC816D3AF');
             $this->addSql('ALTER TABLE event_store DROP FOREIGN KEY FK_BE4CE95BD0BBCCBE');
             $this->addSql('ALTER TABLE event_store DROP FOREIGN KEY FK_BE4CE95B401B253C');
@@ -79,7 +87,7 @@ final class Version20201005084703 extends AbstractMigration
             $this->addSql('DROP TABLE event_store_aggregate_types');
             $this->addSql('DROP TABLE event_store_aggregates');
             $this->addSql('DROP TABLE event_store_event_types');
-        } else if ($platform === SqlitePlatform::class) {
+        } else if ($isSqlite) {
             $this->addSql('DROP TABLE event_store');
             $this->addSql('DROP TABLE event_store_aggregate_types');
             $this->addSql('DROP TABLE event_store_aggregates');
